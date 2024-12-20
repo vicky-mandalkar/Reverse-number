fec0ce770a829ddb7b8f804aa1a929135b0c9b3d
vicky Mandalkar <vickymandalkar143@gmail.com>
2024-12-20 15:21:21 +0530

    Update FibonacciNumber.java

--- FibonacciNumber.java
+++ FibonacciNumber.java
@@ -2,11 +2,15 @@ package Interview_Coding_Question_Practice;
 import java.util.Scanner;
 public class FibonacciNumber {
     public static void main(String[] args) {
+       
         Scanner sc=new Scanner(System.in);
         System.out.println("enter a num:");
         int n=sc.nextInt();
+        
         int  Fnum=0,Snum=1;
+        
         int Nextnum;
+        
         for (int i=1;i<=n;i++){
             System.out.print(Fnum+" ");
             Nextnum=Fnum+Snum;
@@ -16,4 +20,7 @@ public class FibonacciNumber {
 
     }
 }
+// enter a num:
+// 12
+// 0 1 1 2 3 5 8 13 21 34 55 89 
 
