66fd0c376af0229cdff24c2d15e16b0ef6d45acc
vicky Mandalkar <vickymandalkar143@gmail.com>
2024-12-20 15:12:25 +0530

    Update ReverseNumber.java

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,17 +1,25 @@
 package Interview_Coding_Question_Practice;
 
+import java.util.Scanner;
+
 public class ReverseNumber
 {
         public static void main(String[] args)
         {
-            int number = 987654, reverse = 0;
-            while(number != 0)
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter a any number :");
+            int n=sc.nextInt();
+            int reverse = 0;
+            while(n != 0)
             {
-                int remainder = number % 10;
+                int remainder = n % 10;
                 reverse = reverse * 10 + remainder;
-                number = number/10;
+                n = n/10;
             }
             System.out.println("The reverse of the given number is: " + reverse);
         }
     }
 
+// output=Enter a any number :
+// 123456
+// The reverse of the given number is: 654321
